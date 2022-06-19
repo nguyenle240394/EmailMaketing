51193263134cab77274043b3adc45dd0a7eb704d
Nguyen Le <nguyenle.httdn@gmail.com>
2022-06-19 10:42:15 +0700

    .

--- src/EmailMaketing.Application/Customers/CustomerAppService.cs
+++ src/EmailMaketing.Application/Customers/CustomerAppService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Identity;
@@ -10,13 +12,31 @@ namespace EmailMaketing.Customers
     public class CustomerAppService : ApplicationService, ICustomerAppService
     {
         private readonly ICustomerRepository _customerRepository;
-        private readonly IdentityUserManager _userManager;
+        private readonly IdentityUserAppService _identityUserAppService;
+        private readonly IdentityUserManager _identityUserManager;
 
-        public CustomerAppService(ICustomerRepository customerRepository, IdentityUserManager UserManager)
+        public CustomerAppService(ICustomerRepository customerRepository, IdentityUserAppService identityUserAppService)
         {
             _customerRepository = customerRepository;
-            _userManager = UserManager;
+            _identityUserAppService = identityUserAppService;
         }
+
+        public async Task ChangeStatus(Guid Id)
+        {
+            var identityUser = new IdentityUserUpdateDto();
+            var customer = await _customerRepository.FindAsync(Id);
+
+            customer.Status = !customer.Status;
+            await _customerRepository.UpdateAsync(customer);
+            var user = await _identityUserAppService.GetAsync(customer.UserID);
+
+            user.IsActive = !user.IsActive;
+            identityUser.UserName= user.UserName;
+            identityUser.Email = user.Email;
+            identityUser.IsActive = user.IsActive;
+            await _identityUserAppService.UpdateAsync(customer.UserID, identityUser);
+        }
+
         public async Task<CustomerDto> CreateAsync(CreateUpdateCustomer input)
         {
             var customer = ObjectMapper.Map<CreateUpdateCustomer, Customer>(input);
@@ -24,9 +44,10 @@ namespace EmailMaketing.Customers
             return ObjectMapper.Map<Customer, CustomerDto>(customer);
         }
 
-        public Task<bool> DeleteAsync(Guid id)
+        public async Task<bool> DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var customer = await _customerRepository.FindAsync(id);
+            return true;
         }
 
         public Task<CustomerDto> GetCustomerAsync(Guid id)
@@ -42,6 +63,7 @@ namespace EmailMaketing.Customers
                     input.Sorting,
                     input.Filter
                 );
+            
             var customerAdd = ObjectMapper.Map<List<Customer>, List<CustomerDto>>(customers);
             var totalCount = await _customerRepository.GetCountAsync();
             return new PagedResultDto<CustomerDto>(
--- src/EmailMaketing.Web/Pages/Customers/Index.js
+++ src/EmailMaketing.Web/Pages/Customers/Index.js
@@ -1,9 +1,11 @@
-﻿$(function () {
-    var l = abp.localization.getResource('EmailMaketing');
-    /*var createModal = new abp.ModalManager(abp.appPath + 'Customers/CreateModal');*/
+﻿var dataTable;
+var l;
+$(function () {
+    l = abp.localization.getResource('EmailMaketing');
+    var createModal = new abp.ModalManager(abp.appPath + 'Customers/CreateModal');
     /*var editModal = new abp.ModalManager(abp.appPath + 'Customers/EditModal');*/
 
-    var dataTable = $('#CustomerTable').DataTable(
+    dataTable = $('#CustomerTable').DataTable(
         abp.libs.datatables.normalizeConfiguration({
             serverSide: true,
             paging: true,
@@ -12,21 +14,21 @@
             scrollX: true,
             ajax: abp.libs.datatables.createAjax(emailMaketing.customers.customer.getList),
             columnDefs: [
-               /* {
+                {
                     title: l('Actions'),
                     rowAction: {
                         items:
                             [
                                 {
                                     text: l('Edit'),
-                                    visible: abp.auth.isGranted('BachHoaXanh.Customers.Edit'),
+                                    visible: abp.auth.isGranted('EmailMaketing.Customers.Edit'),
                                     action: function (data) {
                                         editModal.open({ id: data.record.id });
                                     }
                                 },
                                 {
                                     text: l('Delete'),
-                                    visible: abp.auth.isGranted('BachHoaXanh.Customers.Delete'),
+                                    visible: abp.auth.isGranted('EmailMaketing.Customers.Delete'),
                                     confirmMessage: function (data) {
                                         return l('CustomerDeletionConfirmationMessage', data.record.name);
                                     },
@@ -46,11 +48,11 @@
                                 }
                             ]
                     }
-                },*/
-               /* {
+                },
+                {
                     title: l('User Name'),
                     data: "userName"
-                },*/
+                },
                 {
                     title: l('Name'),
                     data: "fullName",
@@ -63,6 +65,22 @@
                 {
                     title: l('Email'),
                     data: "email"
+                },
+                {
+                    "orderable": false,
+                    title: l('Status'),
+                    data: { status: "status", id: "id" },
+                    render: function (data) {
+
+                        var check = '';
+                        if (data.status == 1)
+                            check = "checked";
+                        var str = '<label class="switch">' +
+                            `<input type = "checkbox" id="a${data.id}" ${check} onclick="ChangeStatus(this.id,${data.status})">` +
+                            '<span class="slider round"></span>' +
+                            '</label >';
+                        return str;
+                    }
                 },
                 {
                     title: l('CreationTime'), data: "creationTime",
@@ -86,8 +104,48 @@
     });*/
 
 
-    /*$('#NewCustomerButton').click(function (e) {
+    $('#NewCustomerButton').click(function (e) {
         e.preventDefault();
         createModal.open();
-    });*/
+    });
+   
 });
+function ChangeStatus(id, status) {
+    if ($('#a' + id).is(':checked')) {
+        $("#a" + id).prop("checked", false);
+    }
+    else {
+        $("#a" + id).prop("checked", true);
+    }
+    dataTable.ajax.reload();
+    var mess = l('Block The User');
+    if (status == 0) {
+        mess = l('Unlock The User');
+    }
+    console.log(id.substring(1))
+    abp.message.confirm(mess, l('Notify'))
+        .then(function (confirmed) {
+
+            if (confirmed) {
+                emailMaketing.customers.customer.changeStatus(id.substring(1))
+                abp.message.success(l('Successfully'), l('Congratulations'));
+
+                if ($('#a' + id).is(':checked')) {
+                    $("#a" + id).prop("checked", false);
+                }
+                else {
+                    $("#a" + id).prop("checked", true);
+                }
+                dataTable.ajax.reload();
+            }
+            else {
+                if ($('#a' + id).is(':checked')) {
+                    $("#a" + id).prop("checked", false);
+                }
+                else {
+                    $("#a" + id).prop("checked", true);
+                }
+            }
+        });
+    
+};

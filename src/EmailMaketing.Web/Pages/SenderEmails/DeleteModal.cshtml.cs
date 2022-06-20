4cc788dbe21745d5919a42d12355195101b294a1
quynh.tkqpy@gmail.com <quynh.tkqpy@gmail.com>
2022-06-20 14:42:43 +0700

    .

---
+++ src/EmailMaketing.Web/Pages/SenderEmails/DeleteModal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmailMaketing.Web.Pages.SenderEmails
+{
+    public class DeleteModalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

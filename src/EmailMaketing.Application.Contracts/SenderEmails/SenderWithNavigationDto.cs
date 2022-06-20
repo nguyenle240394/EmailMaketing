88bebf4696a7cd5c04f98dcf187380ff1ba3da27
quynh.tkqpy@gmail.com <quynh.tkqpy@gmail.com>
2022-06-20 11:16:23 +0700

    request email

--- src/EmailMaketing.Application.Contracts/SenderEmails/SenderWithNavigationDto.cs
+++ src/EmailMaketing.Application.Contracts/SenderEmails/SenderWithNavigationDto.cs
@@ -1,4 +1,4 @@
-﻿//using EmailMaketing.Customers;
+﻿using EmailMaketing.Customers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +10,6 @@ namespace EmailMaketing.SenderEmails
     public class SenderWithNavigationDto
     {
         public SenderEmailDto SenderEmail { get; set; }
-        //public CustomerDto Customer { get; set; }
+        public CustomerDto Customer { get; set; }
     }
 }

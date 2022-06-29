24d7ca1c230054b2b3f6579350aed0de6211e758
Nguyenle <Nguyenle.httdn@gmail.com>
2022-06-29 16:55:58 +0700

    update STT for customer

--- src/EmailMaketing.Application.Contracts/Customers/CustomerDto.cs
+++ src/EmailMaketing.Application.Contracts/Customers/CustomerDto.cs
@@ -7,6 +7,7 @@ namespace EmailMaketing.Customers
 {
     public class CustomerDto: AuditedEntityDto<Guid>
     {
+        public int Stt { get; set; }
         public Guid UserID { get; set; }
         public string UserName { get; set; }
         public string FullName { get; set; }

ea11e46315417f1e9b5c143521799c2eedb14f12
quynh.tkqpy@gmail.com <quynh.tkqpy@gmail.com>
2022-07-12 14:09:47 +0700

    done hien thi danh sach theo dieu kien cua user

--- src/EmailMaketing.Web/Pages/SenderEmails/Index.js
+++ src/EmailMaketing.Web/Pages/SenderEmails/Index.js
@@ -3,7 +3,7 @@ $(function () {
     l = abp.localization.getResource('EmailMaketing');
     /*var createModal = new abp.ModalManager({
         viewUrl: abp.appPath + 'EmailManagement/SenderEmails/CreateModal',
-        scriptUrl: '/Pages/EmailManagement/SenderEmails/CreateModal.js'
+        scriptUrl: '/Pages/SenderEmails/CreateModal.js'
     });*/
     var createModal = new abp.ModalManager(abp.appPath + 'SenderEmails/CreateModal');
     var editModal = new abp.ModalManager(abp.appPath + 'SenderEmails/EditModal');
@@ -15,7 +15,7 @@ $(function () {
             order: [[1, "asc"]],
             searching: true,
             scrollX: true,
-            ajax: abp.libs.datatables.createAjax(emailMaketing.senderEmails.senderEmail.getListWithNavigation),
+            ajax: abp.libs.datatables.createAjax(emailMaketing.senderEmails.senderEmail.getList),
             columnDefs: [
                 {
                     title: l('No.'),
@@ -23,23 +23,23 @@ $(function () {
                 },
                 {
                     title: l('Email'),
-                    data: "senderEmail.email"
+                    data: "email"
                 },
                 {
                     title: l('Password'),
-                    data: "senderEmail.password"
+                    data: "password"
                 },
                 {
                     title: l('Customer Name'),
-                    data: "customer",
-                    render: function (data) {
+                    data: "customerName"
+                    /*render: function (data) {
                         if (data != null) return data.fullName;
                         return "";
-                    }
+                    }*/
                 },
                 {
                     title: l('IsSend'),
-                    data: "senderEmail.isSend"
+                    data: "isSend"
                 },
                 {
                     title: l('Actions'),

79ec49823b6763e4f6f890b56d14aca988cd0100
Nguyen Le <nguyenle.httdn@gmail.com>
2022-07-18 15:45:17 +0700

    disabled schedule with don't check input time

--- src/EmailMaketing.Web/Pages/ContentEmails/SendEmailModal.js
+++ src/EmailMaketing.Web/Pages/ContentEmails/SendEmailModal.js
@@ -3,17 +3,13 @@
     
 })
 
-$(document).ready(function () {
-    
-})
-
 
 $('#ShowTime').on('click', function () {
     if (this.checked) {
-        
+        document.getElementById("ScheduleJob").disabled = false;
         document.getElementById("ContentEmail_Day").style.display = 'block';
     } else {
-        
+        document.getElementById("ScheduleJob").disabled = true;
         document.getElementById("ContentEmail_Day").style.display = 'none';
     }
 })

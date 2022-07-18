f16ba9e20edac51fde82a6fddddbc8e91f49448a
Nguyenle <Nguyenle.httdn@gmail.com>
2022-07-18 08:30:07 +0700

    disaveld button ScheduleJobs with don't check time

--- src/EmailMaketing.Web/Pages/ContentEmails/SendEmailModal.js
+++ src/EmailMaketing.Web/Pages/ContentEmails/SendEmailModal.js
@@ -5,10 +5,10 @@
 
 $('#ShowTime').on('click', function () {
     if (this.checked) {
-        
         document.getElementById("ContentEmail_Day").style.display = 'block';
+        document.getElementById("ScheduleJob").disabled = false;
     } else {
-        
+        document.getElementById("ScheduleJob").disabled = true;
         document.getElementById("ContentEmail_Day").style.display = 'none';
     }
 })
